b60adeebc6642f0bee49dda25983ad3838d7d501
Philip James de Vries <phil@dvrsdesign.com>
2015-06-25 08:59:02 -0400

    rough file structure

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Soundcloud API</title>
     </head>
     <body>
+        <h1>Soundcloud API</h1>
         <?php
         // put your code here
         ?>
+        <p>Descriptions of project and links to everyone's pages will go here.</p>
     </body>
 </html>
