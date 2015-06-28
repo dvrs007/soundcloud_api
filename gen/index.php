fcd538b5099e0dc9426519aaab8ce7dd2a0b95a0
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-06-28 16:13:19 -0400

    beginnings of tutorial

--- gen/index.php
+++ gen/index.php
@@ -3,6 +3,7 @@
 require_once '../sc_credentials.php';
 require_once '../Services/Soundcloud.php';
 
+//create a client object with your app credentials
 $client = new Services_Soundcloud(
     'CLIENT_ID',
     'CLIENT_SECRET'
@@ -11,13 +12,15 @@ $client = new Services_Soundcloud(
 $query='';
 $results_array=[];
 
+//if a query is posted
 if(isset($_POST['submit'])){
 
 	$query = $_POST['search'];
 
+	//url for GET request
 	$url = 'http://api.soundcloud.com/tracks';
 
-	//find sounds with query keyword
+	//find sounds with query keyword, tack onto the end of url
 	$results = $client->get($url, array(
 		'q' => $query,
 		//'order' => 'created_at', 
