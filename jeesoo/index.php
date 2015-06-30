0c4af6d13cea70472af8538a1316b7f88fe507e3
JeesooKim <jeesoo.joa@gmail.com>
2015-06-29 22:32:44 -0400

    fileupload api

--- jeesoo/index.php
+++ jeesoo/index.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
-
-require_once '../Services/Soundcloud.php';
+require_once 'php-soundcloud-master/Services/Soundcloud.php';
 require_once 'sc_credentials.php';
-include '../views/header.php';
-//Ref: https://github.com/mptre/php-soundcloud/wiki/OAuth-2
 
 // create a client object with your app credentials
 $sc_connection = new Services_Soundcloud(
@@ -20,22 +17,14 @@ $authURL= $sc_connection->getAuthorizeUrl();
 
 echo '<a href="' . $authURL . '">Connect to SoundCloud</a>';
 
-echo '<pre>';
-
-
-
-//Attept to get token from Session First
-//Set the token otherwise
-//Ref: http://stackoverflow.com/questions/18919021/soundcloud-the-requested-url-responded-with-http-code-0
 try {
-	//$accessToken=$sc_connection->setCurlOptions();
-		
 	if(!isset($_SESSION['token'])){
 		$accessToken = $sc_connection->accessToken($_GET['code'],array(),array(
 			CURLOPT_SSL_VERIFYPEER => false,
 			CURLOPT_SSL_VERIFYHOST => false,
 			));
 		$_SESSION['token']=$accessToken['access_token'];
+		
 	}
 	else
 	{
@@ -47,38 +36,29 @@ try {
     exit($e->getMessage());
 }
 
-/*
+
 try {
     $me = json_decode($sc_connection->get('me'), true);
-	print_r($me);
-	echo '<br/> ID:' . $me['id'];
+	//print_r($me);
+	//echo '<br/> ID:' . $me['id'];
 		
-	echo '<br/> My Tracks <br/>';
-	$tracks = json_decode($sc_connection->get('tracks',array('user_id' => $me['id'])), true);
-	print_r($tracks);
+	//echo '<br/> My Tracks <br/>';
+	//$tracks = json_decode($sc_connection->get('tracks',array('user_id' => $me['id'])), true);
+	//print_r($tracks);
 	
 	
 } catch (Services_Soundcloud_Invalid_Http_Response_Code_Exception $e) {
     exit($e->getMessage());
-}*/
-
-try{
-	
-	$track= $sc_connection->post('tracks', array(
-		'track[asset_data]'=> '@bwv866_06272015.wav',
-		'track[title]'=> 'TEST: BWV866',
-		'track[sharing]'=> 'public'
-	
-	));
-	
-}catch(Services_Soundcloud_Invalid_Http_Response_Code_Exception $e){
-	echo $e->getMessage();
 }
 
 
-
-echo '</pre>';
-
-
-
-?>
+     
+    $user_data = array(
+                    'access_token' => $accessToken['access_token'],
+                    'id' => $me['id'],
+                    'username' => $me['username'],
+                    'name' => $me['full_name'],
+                    'avatar' => $me['avatar_url']
+                );
+     
+    header("Location:upload.php?access_token=".$user_data['access_token']);
