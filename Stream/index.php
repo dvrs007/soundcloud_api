4738a3c0aa42aaf432d332d94b67a1cc9eb904df
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-06-30 18:34:33 -0400

    fix header stream
    remove all the html tags and included php include

--- Stream/index.php
+++ Stream/index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Soundcloud API Tutorials</title>
+<?php 
 
-        <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="../css/style.css" />
-        <link rel="stylesheet" type="text/css" href="../css/styles/atelier-cave.light.css" />
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-        <!-- jQuery -->
-        <script src="/soundcloud_api/js/jquery-2.1.4.js"></script>
-
-
-
-
+include "../views/header.php";
+?>
 
     
 <script src="//connect.soundcloud.com/sdk.js">
@@ -69,33 +56,6 @@ $('#play').click(function(e) {
    
     
 </script>
-        </head>
-    <body>
-
- <header> 
-        <div id="navbar">
-            <nav>
-            	<div class="home-btn">
-					<a href="../index.php"><i class="fa fa-home"></i></a>
-				</div>
-            	<ul>
-	                <li class="tutorials"><i class="fa fa-caret-down"></i> API Tutorials
-	                	<div class="secondary">
-	                		<ul>
-		                			<li><a href="../soundcloud_api/search/index.php" title="Search Tutorial">Search SoundCloud</a></li>
-		                		<li><a href="../soundcloud_api/upload/" title="Upload Tutorial">Upload your sound</a></li>
-		                		<li><a href="../soundcloud_api/Stream/" title="">Stream Tutorial</a></li>
-		                		<li><a href="#" title="">Track Widget Tutorial</a></li>
-	                		</ul>
-	                	</div>
-	                </li>
-	                <li class="sc-link"><a href="https://developers.soundcloud.com/" target="_blank"><i class="fa fa-soundcloud"></i> SoundCloud</a></li>
-                </ul>
-             </nav>
-        </div>
-    </header>
-
-    <div class="container"> 
 
    
 <h1>Streaming with JavaScript</h1>
