fcde333c8e801142e66384095a92811af8aaec53
johnsonta87 <johnsonta87@gmail.com>
2015-06-29 23:49:35 -0400

    soundcloud play widget

--- johnson/play_widget.php
+++ johnson/play_widget.php
@@ -1,27 +1,14 @@
-<script src="https://w.soundcloud.com/player/api.js" type="text/javascript"></script>
+<?php
+require_once 'Services/Soundcloud.php';
+require_once 'sc_credentials.php';
 
-<iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites" width="100%" height="465" scrolling="no" frameborder="no"></iframe>
-<script src="https://w.soundcloud.com/player/api.js" type="text/javascript"></script>
-<script type="text/javascript">
-  (function(){
-    var widgetIframe = document.getElementById('sc-widget'),
-        widget       = SC.Widget(widgetIframe);
+// create a client object with your app credentials
+$client = new Services_Soundcloud($clientID, $clientSecret);
+$client->setCurlOptions(array(CURLOPT_FOLLOWLOCATION => 1));
 
-    widget.bind(SC.Widget.Events.READY, function() {
-      widget.bind(SC.Widget.Events.PLAY, function() {
-        // get information about currently playing sound
-        widget.getCurrentSound(function(currentSound) {
-          console.log('sound ' + currentSound.get('') + 'began to play');
-        });
-      });
-      // get current level of volume
-      widget.getVolume(function(volume) {
-        console.log('current volume value is ' + volume);
-      });
-      // set new volume level
-      widget.setVolume(50);
-      // get the value of the current position
-    });
+// get a tracks oembed data
+$track_url = 'http://soundcloud.com/forss/flickermood';
+$embed_info = json_decode($client->get('oembed', array('url' => $track_url)));
 
-  }());
-</script>
+// render the html for the player widget
+print $embed_info->html;
