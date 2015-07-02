cddeb45e23d3a68bead7287d53ef55c622065f8e
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-07-02 18:15:45 -0400

    Merge branch 'master' into gen
    Conflicts:
    	views/header.php

--- views/header.php
+++ views/header.php
@@ -34,7 +34,11 @@
 		                		<li><a href="../search/index.php" title="Search Tutorial">Search SoundCloud</a></li>
 		                		<li><a href="../upload/index.php" title="Upload Tutorial">Upload your sound</a></li>
 		                		<li><a href="../Stream/index.php" title="">Stream Tutorial</a></li>
+<<<<<<< HEAD
 		                		<li><a href="../kaitlin/index.php" title="">Track Widget Tutorial</a></li>
+=======
+		                		<li><a href="../johnson/index.php" title="">Track Widget Tutorial</a></li>
+>>>>>>> master
 	                		</ul>
 	                	</div>
 	                </li>
