58e4ac2f66eb1259aaca78fe9bd4934b05a72523
JeesooKim <jeesoo.joa@gmail.com>
2015-07-03 11:54:50 -0400

    upload_credentials with new file

--- jeesoo/upload_credentials.php
+++ jeesoo/upload_credentials.php
@@ -4,10 +4,4 @@ define( 'CLIENT_ID' , '1a8c7c2ef2112d424097128f312b8e0d');
 define( 'CLIENT_SECRET' , '7f9e233feb4cb56b0c7a9023ea6abf12');
 define( 'CALL_BACK_URI', 'http://ijeesoo.com/WebTrends/SoundCloud/');
 
-
-/*
-define( 'CLIENT_ID' , '4a562b1152ae828060d7bfbe66343599');
-define( 'CLIENT_SECRET' , '393b582e4316c78a7e1b5c7196e2badc');
-define( 'CALL_BACK_URI', 'http://soundcloudapi.philipjamesdevries.com/jeesoo/');
-*/
 ?>
