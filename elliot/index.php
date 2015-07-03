071c345a9e2352f7c53f0e1919470a3ce3ce7bb2
Elliot Grabish <egrabish@gmail.com>
2015-07-03 10:29:12 -0400

    removed uploads (elliot)

--- elliot/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-	<form method="POST" action="http://www.moosenado.com/soundcloudapi/accept.php">
-		<input type="submit" value="Log In" name="login" id="login" />
-	</form>
-</body>
-</html>
